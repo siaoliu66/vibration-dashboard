cf3205b9225a1d1f9eaa424f3b6ee570dbb5a3ea
siaoliu66 <siaoliu66@gmail.com>
2026-07-01 12:37:22 +0800

    feat: vibration monitoring dashboard

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  base: '/vibration-dashboard/'   // 加這行，名稱要跟你的 repo 名稱一樣
+});
